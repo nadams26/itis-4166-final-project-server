80a9ab933bd1cca45e038db480559684752421d6
Nathan Adams <nadams26@uncc.edu>
2020-12-13 23:18:47 -0500

    changed port to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const app = express()
-const port = 3001
+const port = 3000
 
 var connection = mysql.createConnection({
     host : 'sql9.freemysqlhosting.net',
